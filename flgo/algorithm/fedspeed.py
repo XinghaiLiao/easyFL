eb483ce3b1bb04a70cc6febf35af1306a1b19d8f
zzz <zzz510711928@gmail.com>
2024-07-27 17:06:25 +0800

    add fedspeed

--- flgo/algorithm/fedspeed.py
+++ flgo/algorithm/fedspeed.py
@@ -1,3 +1,4 @@
+import copy
 from .fedbase import BasicServer
 from .fedbase import BasicClient
 import flgo.utils.fmodule as fmodule
@@ -5,8 +6,13 @@ import torch
 
 class Server(BasicServer):
     def initialize(self, *args, **kwargs):
-        self.init_algo_para({"lmbd": 0.1})
-
+        """
+        lmbd: 0.001, 0.01, 0.1, 0.5
+        alpha: 0.0, 0.5, 0.75, 0.875, 0.9375, 1.0
+        rho: 0.1
+        """
+        self.init_algo_para({"lmbd": 0.1, 'alpha':0.9375, 'rho':0.1})
+        self.aggregation_option = 'uniform'
 class Client(BasicClient):
     def initialize(self, *args, **kwargs):
         self.local_grad_controller = self.server.model.zeros_like().to('cpu')
@@ -21,16 +27,35 @@ class Client(BasicClient):
             model (FModule): the global model
         """
         model.train()
-        optimizer = self.calculator.get_optimizer(model, lr=self.learning_rate, weight_decay=self.weight_decay,
-                                                  momentum=self.momentum)
+        model_tmp = copy.deepcopy(model)
+        global_model = copy.deepcopy(model)
+        global_model.freeze_grad()
+        self.local_grad_controller.to(self.device)
+        optimizer = self.calculator.get_optimizer(model, lr=self.learning_rate, weight_decay=self.weight_decay, momentum=self.momentum)
+        optimizer_tmp = self.calculator.get_optimizer(model_tmp, lr=self.learning_rate, weight_decay=self.weight_decay, momentum=self.momentum)
         for iter in range(self.num_steps):
             # get a batch of data
+            optimizer.zero_grad()
+            optimizer_tmp.zero_grad()
             batch_data = self.get_batch_data()
-            model.zero_grad()
             # calculate the loss of the model on batched dataset through task-specified calculator
-            loss = self.calculator.compute_loss(model, batch_data)['loss']
-            loss.backward()
-            if self.clip_grad > 0: torch.nn.utils.clip_grad_norm_(parameters=model.parameters(),
-                                                                  max_norm=self.clip_grad)
+            loss1 = self.calculator.compute_loss(model_tmp, batch_data)['loss']
+            loss1.backward()
+            grad1 = [p.grad for p in model_tmp.parameters()]
+            optimizer_tmp.step()
+            optimizer_tmp.zero_grad()
+            loss2 = self.calculator.compute_loss(model_tmp, batch_data)['loss']
+            loss2.backward()
+            grad2 = [p.grad for p in model_tmp.parameters()]
+            quasi_grad = [(1-self.alpha)*g1+self.alpha*g2 if g1 is not None else None for g1, g2 in zip(grad1, grad2)]
+            with torch.no_grad():
+                for p,qg,lg,m in zip(model.parameters(), quasi_grad, self.local_grad_controller.parameters(), global_model.parameters()):
+                    p.grad = qg - lg + 1.0/self.lmbd*(p.detach() -  m)
+            if self.clip_grad > 0: torch.nn.utils.clip_grad_norm_(parameters=model.parameters(), max_norm=self.clip_grad)
             optimizer.step()
+        self.local_grad_controller = self.local_grad_controller - 1.0/self.lmbd * (model - global_model)
+        with torch.no_grad():
+            for pm, lg in zip(model.parameters(), self.local_grad_controller.parameters()):
+                pm.data = pm.data - self.lmbd*lg
+        self.local_grad_controller.to('cpu')
         return
