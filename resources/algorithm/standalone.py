d2a767af1d0259554aaad3056dbdc88a17898e06
zzz <zzz510711928@gmail.com>
2024-09-23 23:34:14 +0800

    update standalone

--- resources/algorithm/standalone.py
+++ resources/algorithm/standalone.py
@@ -1,10 +1,36 @@
 import copy
-
-import torch
+import os
+import torch.nn as nn
+import numpy as np
+import torch.nn.functional as F
 from tqdm import trange
 import flgo.algorithm.fedbase as fab
-
+from torch.optim import lr_scheduler
+import flgo.algorithm.fedavg as fedavg
+import flgo.utils.fmodule as fuf
+from torch.utils.data import Dataset
+from torchvision.transforms import RandomCrop, RandomHorizontalFlip
+import torchvision
+import torch
+import utils.submodule as usub
+from utils.pdmodule import *
+MIN_RATIO = 0.0625
 class Server(fab.BasicServer):
+    def initialize(self, *args, **kwargs):
+        self.init_local_model()
+
+    def init_local_model(self):
+        def get_model_size(model):
+            return sum(p.numel() for p in model.parameters()) * 4 / (1024 ** 2)
+        total_size = get_model_size(self.model)
+        all_p = np.arange(MIN_RATIO, 1.001, MIN_RATIO)
+        size_ratio = [1.0*get_model_size(self.model.__class__(p))/total_size for p in all_p]
+        for c in self.clients:
+            pidx = 0
+            while pidx+1 < len(all_p) and (size_ratio[pidx+1] <= c._capacity): pidx += 1
+            c.p = all_p[pidx]
+            c.model = self.model.__class__(c.p)
+
     def run(self):
         tune_key, tune_direction = self.gv.logger.get_es_key(), self.gv.logger.get_es_direction()
         tune_key = tune_key.split('_')[-1]
@@ -27,11 +53,9 @@ class Server(fab.BasicServer):
         return
 
 class Client(fab.BasicClient):
-    def initialize(self, *args, **kwargs):
-        self.model = copy.deepcopy(self.server.model)
-
     def finetune(self):
         dataloader = self.calculator.get_dataloader(self.train_data, batch_size=self.batch_size)
+        self.model.to(self.device)
         optimizer = self.calculator.get_optimizer(self.model, lr=self.learning_rate, weight_decay=self.weight_decay, momentum=self.momentum)
         epoch_iter = trange(self.num_epochs+1)
         op_model_dict = copy.deepcopy(self.model.state_dict())
@@ -63,5 +87,3 @@ class Client(fab.BasicClient):
                 epoch_iter.set_description("\t".join(["Client {}".format(self.id), "Optimal Epoch {}/{}".format(op_epoch, self.num_epochs)] + ['{}: {:.4f}'.format(k, v) for k, v in op_met.items()]))
                 self.model.load_state_dict(op_model_dict)
         return
-
-
