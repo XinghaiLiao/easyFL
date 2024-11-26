d59caf31f9a931669c592377128e6c2f5760cd99
WwZzz <zwang@stu.xmu.edu.cn>
2024-11-26 11:47:24 +0800

    update example

--- test.py
+++ test.py
@@ -1,30 +1,26 @@
+import flgo
 import flgo.algorithm.fedavg as fedavg
-import flgo.experiment.analyzer
-import os
+import flgo.experiment.analyzer as fea
+import flgo.benchmark.mnist_classification as mnist
+import flgo.benchmark.partition as fbp
 
 task = './my_task'
-gen_config = {
-    'benchmark':{'name':'flgo.benchmark.mnist_classification'},
-    'partitioner':{'name':'IIDPartitioner', 'para':{'num_clients':100}}
-}
-analysis_plan = {
-    'Selector':{'task':'./my_task', 'header':['fedavg',], },
-    'Painter':{
-        'Curve':[
-            {
+if __name__ == '__main__':
+    # generate federated task (remark: if task already exists, this line will not work)
+    flgo.gen_task_by_(mnist, fbp.IIDPartitioner(num_clients=100), task_path=task)
+
+    # running fedavg on the specified task
+    runner = flgo.init(task, fedavg, {'gpu':[0,],'log_file':True, 'num_epochs':1})
+    runner.run()
+
+    # visualize the experimental result
+    records = fea.load_records(task, 'fedavg', {'num_epochs':1})
+    fea.Painter(records).create_figure(fea.Curve,{
                 'args':{'x':'communication_round', 'y':'val_loss'},
                 'obj_option':{'color':'r'},
                 'fig_option':{'xlabel': 'communication round', 'ylabel':'val_loss', 'title':'fedavg on {}'.format(task)}
             }
-        ]
-    },
-    'Table':{'min_value':[{'x':'val_loss'}]},
-}
-if __name__ == '__main__':
-    # generate federated task if task doesn't exist
-    if not os.path.exists(task): flgo.gen_task(gen_config, task_path=task)
-    # running fedavg on the specified task
-    runner = flgo.init(task, fedavg, {'gpu':[0,],'log_file':True, 'num_epochs':1})
-    runner.run()
-    # visualize the experimental result
-    flgo.experiment.analyzer.show(analysis_plan)
+    )
+    table = fea.Table(records)
+    table.add_column(fea.min_value, {'x':'val_loss'})
+    table.print()
